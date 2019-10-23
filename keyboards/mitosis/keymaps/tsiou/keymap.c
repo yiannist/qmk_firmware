9c86d9d9524ef64bdea9a9c47cd9ad9cbf26d6cc
Yiannis Tsiouris <tsiou@skroutz.gr>
2019-10-23 09:52:30 +0300

    Change ralt-shift

--- keyboards/mitosis/keymaps/tsiou/keymap.c
+++ keyboards/mitosis/keymaps/tsiou/keymap.c
@@ -14,21 +14,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *  q    w      e      r      t        ||        y     u    i     o     p
    *  a    s      d      f      g        ||        h     j    k     l     ç
    *  z    x      c      v      b        ||        n     m    ,     .     ;
-   *      esc    FΝ2    lalt   ctrl      ||       alt   del  ins  enter
-   *      tab   shift  super   bksp      ||      space  FN1   ´     ~
+   *      esc    FΝ2   shift   ctrl      ||       alt   del  ins  enter
+   *      tab    ralt  super   bksp      ||      space  FN1   ´     ~
    */
   [_xQ] = LAYOUT(
       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,
       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,
       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,
-      /*,   */ KC_ESC,  MO(_xF), KC_RALT, KC_LCTL,     KC_LALT, KC_DEL,  KC_INS,  KC_ENT,
-      /*,   */ KC_TAB,  KC_LSFT, KC_LGUI, KC_BSPC,     KC_SPC,  MO(_xN), KC_LBRC, KC_QUOT),
+      /*,   */ KC_ESC,  MO(_xF), KC_LSFT, KC_LCTL,     KC_LALT, KC_DEL,  KC_INS,  KC_ENT,
+      /*,   */ KC_TAB,  KC_RALT, KC_LGUI, KC_BSPC,     KC_SPC,  MO(_xN), KC_LBRC, KC_QUOT),
   /*
    *  !     @      up     {     }        ||     pgup    7     8     9    *
    *  #    left   down  right   $        ||     pgdn    4     5     6    +
    *  [     ]      (      )     &        ||       '     1     2     3    \
-   *       esc    FΝ2    lalt  ctrl      ||      alt   del   ins    =
-   *       tab   shift  super  bksp      ||     space  FN1    -     0
+   *       esc    FΝ2   shift  ctrl      ||      alt   del   ins    =
+   *       tab    ralt  super  bksp      ||     space  FN1    -     0
    */
   [_xN] = LAYOUT(
       KC_EXLM, KC_AT,   KC_UP,   KC_RCBR,  KC_PIPE,    KC_PGUP, KC_7,    KC_8,    KC_9,     KC_ASTR,
@@ -40,9 +40,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *  !     @      up     <     >        ||     vol+    F7    F8    F9    F10
    *  #    left   down  right   $        ||     vol-    F4    F5    F6    F11
    *  [     ]      (      )     &        ||      -      F1    F2    F3    F12
-   *      reset   FΝ2    lalt  ctrl      ||     alt     del   ins  enter
-   *       tab   shift  super  bksp      ||    space    FN1    ´     ~
-
+   *      reset   FΝ2   shift  ctrl      ||     alt     del   ins  enter
+   *       tab    ralt  super  bksp      ||    space    FN1    ´     ~
    */
   [_xF] = LAYOUT(
       KC_EXLM, KC_AT,   KC_UP,   KC_LT,    KC_GT,       KC_VOLU, KC_F7,   KC_F8,   KC_F9,    KC_F10,
